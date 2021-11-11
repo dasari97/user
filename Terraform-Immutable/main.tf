9320a4cb8dc65ee12a1c02fcca7a43164610b910
dasari97 <dasarisaikrishna97@gmail.com>
2021-11-11 11:44:24 +0000

    adding new file

--- Terraform-Immutable/main.tf
+++ Terraform-Immutable/main.tf
@@ -1,10 +1,13 @@
 module "user" {
-    source = "git::https://dasarisaikrishna97@dev.azure.com/dasarisaikrishna97/Roboshop/_git/immutable//APP-module"
+    source = "git::https://dasarisaikrishna97@dev.azure.com/dasarisaikrishna97/Roboshop/_git/immutable//app-module"
     env = var.env
     instance_type = var.instance_type
     component   = "user"
     port        = 8080
-#    is_internal = "true"
-#    priority    = 200
+    is_internal = "true"
+    priority    = 200
     APP_VERSION = var.APP_VERSION
+    max_size    = var.max_size
+    min_size    = var.min_size
+    desired_capacity    = var.desired_capacity
 }
--- Terraform-Immutable/vars.tf
+++ Terraform-Immutable/vars.tf
@@ -3,3 +3,6 @@ variable "instance_type" {}
 variable "APP_VERSION" {
     default = "null"
 }
+variable "max_size" {}
+variable "min_size" {}
+variable "desired_capacity" {}
